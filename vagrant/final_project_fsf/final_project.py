85e5d8c6b3126ede0027625a25e596337a36911f
Mike McKenna <junkmckenn@gmail.com>
2016-05-02 11:47:58 -0400

    Completed add routes task for final project

---
+++ vagrant/final_project_fsf/final_project.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///finalproject_fsf.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+app = Flask(__name__)
+
+# Application Routes
+
+
+@app.route('/')
+@app.route('/restaurants')
+def showRestaurants():
+    return "This page will show all my restaurants"
+
+
+@app.route('/restaurant/new')
+def newRestaurant():
+    return "This page will be for making a new restaurant"
+
+
+@app.route('/restaurant/<int:restaurant_id>/edit')
+def editRestaurant(restaurant_id):
+    return "This page will be for editing restaurant %s" % restaurant_id
+
+
+@app.route('/restaurant/<int:restaurant_id>/delete')
+def deleteRestaurant(restaurant_id):
+    return "This page will be for deleting restaurant %s" % restaurant_id
+
+
+@app.route('/restaurant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def showMenu(restaurant_id):
+    return "This page is the menu for restaurant %s" % restaurant_id
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def newMenuItem(restaurant_id):
+    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "This page is for editing menu item %s" % menu_id
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "This page is for deleting menu item %s" % menu_id
+
+
+# JSON API Endpoints
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
