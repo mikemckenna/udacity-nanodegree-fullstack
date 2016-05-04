67389be88a832f96b4f35b879fcf111eea855fad
Mike McKenna <junkmckenn@gmail.com>
2016-05-04 12:58:23 -0400

    Added templates and bootstrap styling, list restaurants & add restaurants working

--- vagrant/final_project_fsf/final_project.py
+++ vagrant/final_project_fsf/final_project.py
@@ -18,43 +18,89 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
-    return "This page will show all my restaurants"
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants'}]
+    restaurants = session.query(Restaurant).all()
+    return render_template('restaurants.html', breadcrumbs=breadcrumbs, restaurants=restaurants)
 
 
-@app.route('/restaurant/new')
+@app.route('/restaurant/new', methods=['GET', 'POST'])
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'New'}]
+    if request.method == 'POST':
+        # print request.form
+        # print request.args
+        # print request.values
+        newRestaurant = Restaurant(name=request.form['name'])
+        session.add(newRestaurant)
+        session.commit()
+        flash("New restaurant has been added!")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('restaurant_new.html', breadcrumbs=breadcrumbs)
 
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return "This page will be for editing restaurant %s" % restaurant_id
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'Edit'}]
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('restaurant_edit.html', breadcrumbs=breadcrumbs, restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return "This page will be for deleting restaurant %s" % restaurant_id
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'Delete'}]
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('restaurant_delete.html', breadcrumbs=breadcrumbs, restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'Menu'}]
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', breadcrumbs=breadcrumbs, restaurant=restaurant, items=items)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'Menu', 'href': '/restaurant/<int:restaurant_id>/menu/new'},
+                   {'text': 'New Item'}]
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('menu_item_new.html', breadcrumbs=breadcrumbs, restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return "This page is for editing menu item %s" % menu_id
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'Menu', 'href': '/restaurant/<int:restaurant_id>/menu/new'},
+                   {'text': 'Edit Item'}]
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(menu_id=menu_id).one()
+    return render_template('menu_item_edit.html', breadcrumbs=breadcrumbs, restaurant=restaurant, item=item)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-    return "This page is for deleting menu item %s" % menu_id
+    breadcrumbs = [{'text': 'Home', 'href': '/'},
+                   {'text': 'Restaurants', 'href': '/restaurants'},
+                   {'text': 'Menu', 'href': '/restaurant/<int:restaurant_id>/menu/new'},
+                   {'text': 'Delete Item'}]
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(menu_id=menu_id).one()
+    return render_template('menu_item_delete.html', breadcrumbs=breadcrumbs, restaurant=restaurant, item=item)
 
 
 # JSON API Endpoints
