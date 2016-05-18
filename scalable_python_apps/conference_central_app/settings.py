5bedf70b9a47c0d8620821abe6c1b866c69b762f
Mike McKenna <junkmckenn@gmail.com>
2016-05-18 10:05:16 -0400

    Completed ToDo 1: update saveProfile()

--- scalable_python_apps/conference_central_app/settings.py
+++ scalable_python_apps/conference_central_app/settings.py
@@ -13,4 +13,3 @@ created/forked from conference.py by wesc on 2014 may 24
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
 WEB_CLIENT_ID = 'replace with Web client ID'
-
