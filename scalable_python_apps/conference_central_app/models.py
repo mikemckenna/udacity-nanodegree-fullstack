6be1f8ff67d39969063bcdce509fc6ecea081ad8
Mike McKenna <junkmckenn@gmail.com>
2016-05-24 13:19:41 -0400

    Completed lesson 5 for conference project

--- scalable_python_apps/conference_central_app/models.py
+++ scalable_python_apps/conference_central_app/models.py
@@ -18,9 +18,13 @@ from protorpc import messages
 from google.appengine.ext import ndb
 
 
+class ConflictException(endpoints.ServiceException):
+    """ConflictException -- exception mapped to HTTP 409 response"""
+    http_status = httplib.CONFLICT
+
+
 class Profile(ndb.Model):
     """Profile -- User profile object"""
-    # userId = ndb.StringProperty()
     displayName = ndb.StringProperty()
     mainEmail = ndb.StringProperty()
     teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
@@ -35,10 +39,55 @@ class ProfileMiniForm(messages.Message):
 
 class ProfileForm(messages.Message):
     """ProfileForm -- Profile outbound form message"""
-    userId = messages.StringField(1)
-    displayName = messages.StringField(2)
-    mainEmail = messages.StringField(3)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 4)
+    displayName = messages.StringField(1)
+    mainEmail = messages.StringField(2)
+    teeShirtSize = messages.EnumField('TeeShirtSize', 3)
+    conferenceKeysToAttend = messages.StringField(4, repeated=True)
+
+
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    data = messages.StringField(1, required=True)
+
+
+class BooleanMessage(messages.Message):
+    """BooleanMessage-- outbound Boolean value message"""
+    data = messages.BooleanField(1)
+
+
+class Conference(ndb.Model):
+    """Conference -- Conference object"""
+    name = ndb.StringProperty(required=True)
+    description = ndb.StringProperty()
+    organizerUserId = ndb.StringProperty()
+    topics = ndb.StringProperty(repeated=True)
+    city = ndb.StringProperty()
+    startDate = ndb.DateProperty()
+    month = ndb.IntegerProperty()  # TODO: do we need for indexing like Java?
+    endDate = ndb.DateProperty()
+    maxAttendees = ndb.IntegerProperty()
+    seatsAvailable = ndb.IntegerProperty()
+
+
+class ConferenceForm(messages.Message):
+    """ConferenceForm -- Conference outbound form message"""
+    name = messages.StringField(1)
+    description = messages.StringField(2)
+    organizerUserId = messages.StringField(3)
+    topics = messages.StringField(4, repeated=True)
+    city = messages.StringField(5)
+    startDate = messages.StringField(6)  # DateTimeField()
+    month = messages.IntegerField(7, variant=messages.Variant.INT32)
+    maxAttendees = messages.IntegerField(8, variant=messages.Variant.INT32)
+    seatsAvailable = messages.IntegerField(9, variant=messages.Variant.INT32)
+    endDate = messages.StringField(10)  # DateTimeField()
+    websafeKey = messages.StringField(11)
+    organizerDisplayName = messages.StringField(12)
+
+
+class ConferenceForms(messages.Message):
+    """ConferenceForms -- multiple Conference outbound form message"""
+    items = messages.MessageField(ConferenceForm, 1, repeated=True)
 
 
 class TeeShirtSize(messages.Enum):
@@ -60,41 +109,6 @@ class TeeShirtSize(messages.Enum):
     XXXL_W = 15
 
 
-class Conference(ndb.Model):
-    """Conference -- Conference object"""
-    name = ndb.StringProperty(required=True)
-    description = ndb.StringProperty()
-    organizerUserId = ndb.StringProperty()
-    topics = ndb.StringProperty(repeated=True)
-    city = ndb.StringProperty()
-    startDate = ndb.DateProperty()
-    month = ndb.IntegerProperty()
-    endDate = ndb.DateProperty()
-    maxAttendees = ndb.IntegerProperty()
-    seatsAvailable = ndb.IntegerProperty()
-
-
-class ConferenceForm(messages.Message):
-    """ConferenceForm -- Conference outbound form message"""
-    name = messages.StringField(1)
-    description = messages.StringField(2)
-    organizerUserId = messages.StringField(3)
-    topics = messages.StringField(4, repeated=True)
-    city = messages.StringField(5)
-    startDate = messages.StringField(6)
-    month = messages.IntegerField(7, variant=messages.Variant.INT32)
-    maxAttendees = messages.IntegerField(8, variant=messages.Variant.INT32)
-    seatsAvailable = messages.IntegerField(9, variant=messages.Variant.INT32)
-    endDate = messages.StringField(10)
-    websafeKey = messages.StringField(11)
-    organizerDisplayName = messages.StringField(12)
-
-
-class ConferenceForms(messages.Message):
-    """ConferenceForms -- multiple Conference outbound form message"""
-    items = messages.MessageField(ConferenceForm, 1, repeated=True)
-
-
 class ConferenceQueryForm(messages.Message):
     """ConferenceQueryForm -- Conference query inbound form message"""
     field = messages.StringField(1)
@@ -105,13 +119,3 @@ class ConferenceQueryForm(messages.Message):
 class ConferenceQueryForms(messages.Message):
     """ConferenceQueryForms -- multiple ConferenceQueryForm inbound form message"""
     filters = messages.MessageField(ConferenceQueryForm, 1, repeated=True)
-
-# needed for conference registration
-class BooleanMessage(messages.Message):
-    """BooleanMessage-- outbound Boolean value message"""
-    data = messages.BooleanField(1)
-
-
-class ConflictException(endpoints.ServiceException):
-    """ConflictException -- exception mapped to HTTP 409 response"""
-    http_status = httplib.CONFLICT
